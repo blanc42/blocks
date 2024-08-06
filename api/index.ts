bee75b5de23982b2381c53ec8ca6f2695bc6b40b
blanc42 <85779539+blanc42@users.noreply.github.com>
2024-08-06 08:11:11 +0530

    updated index.ts api function

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,3 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query;
-  res.status(200).json({ message: `Hello, ${name}!` });
-}
+export function GET(request: Request) {
+    return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+  }
